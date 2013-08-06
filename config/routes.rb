ff10e47afe18d898971cfdbf3b4e2bebd0485201
ed wassermann <vis0vis@gmail.com>
2013-08-06 20:07:28 +0200

    establish top white line divider and interviewee top header

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 StartupDnage::Application.routes.draw do
   root to: 'high_voltage/pages#show', id: 'homepage' 
 
-  get 'pages/profile' => 'high_voltage/pages#profile', id: 'profile'
+  get 'pages/profile'      => 'high_voltage/pages#profile', id: 'profile'
+  get 'pages/interviewees' => 'high_voltage/pages#interviewees', id: 'interviewees'
 end
