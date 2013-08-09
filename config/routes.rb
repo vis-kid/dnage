e5194b70c05bac15c341009c01e308ffc5d3f0d7
ed wassermann <vis0vis@gmail.com>
2013-08-09 18:07:30 +0200

    adds about, contact, documentary pages and corrects _navbar partial markup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ StartupDnage::Application.routes.draw do
 
   get 'pages/profile'      => 'high_voltage/pages#profile', id: 'profile'
   get 'pages/interviewees' => 'high_voltage/pages#interviewees', id: 'interviewees'
+  
+  get 'pages/documentary'      => 'high_voltage/pages#documentary', id: 'documentary'
+  get 'pages/about'      => 'high_voltage/pages#about', id: 'about'
+  get 'pages/contact'      => 'high_voltage/pages#contact', id: 'contact'
 end
